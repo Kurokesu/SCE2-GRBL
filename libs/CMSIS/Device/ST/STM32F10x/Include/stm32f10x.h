fdbce42377b46ae2299effe3f931f739ab338784
Danius Kalvaitis <danius.kalvaitis@gmail.com>
2025-01-15 23:58:38 +0200

    move hse value to be set by build system

--- libs/CMSIS/Device/ST/STM32F10x/Include/stm32f10x.h
+++ libs/CMSIS/Device/ST/STM32F10x/Include/stm32f10x.h
@@ -122,9 +122,7 @@
  #ifdef STM32F10X_CL   
   #define HSE_VALUE    ((uint32_t)25000000) /*!< Value of the External oscillator in Hz */
  #else 
-  // SCE2
-  //#define HSE_VALUE    ((uint32_t)1200000) /*!< Value of the External oscillator in Hz */
-  #define HSE_VALUE    ((uint32_t)800000) /*!< Value of the External oscillator in Hz */
+  #define HSE_VALUE    ((uint32_t)8000000U) /*!< Value of the External oscillator in Hz */
  #endif /* STM32F10X_CL */
 #endif /* HSE_VALUE */
 
